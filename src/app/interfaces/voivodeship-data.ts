646e26ab017a5514f239b4d93e22999f1f6ee402
makummba <rozbitejajeczko@gmail.com>
2021-01-23 07:53:13 +0000

    Deploying to gh-pages from master @ 8ba0226153e10686652b43ed64f49d4d0b31ef89 🚀

--- src/app/interfaces/voivodeship-data.ts
+++
@@ -1,9 +0,0 @@
-export interface  VoivodeshipData {
-  voivodeship: string;
-  numberOfInfection: number;
-  numberFor10Citizens: number;
-  numberOfDeaths: number;
-  numberOfDeathsWithOtherSick: number;
-  numberOfDeathsWithoutOtherSick: number;
-
-}
